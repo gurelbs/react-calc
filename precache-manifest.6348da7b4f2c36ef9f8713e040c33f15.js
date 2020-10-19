b24cd6e1e99cf6c5ffcbe7bb3a81130254b000f9
gurelbs <guri240@gmail.com>
2020-10-19 17:50:28 +0300

    Updates

---
+++ precache-manifest.6348da7b4f2c36ef9f8713e040c33f15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4215f4f44de7377f782142e830714696",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "4fe58795b9e78cb32d7f",
+    "url": "/react-calc/static/css/main.85852ad6.chunk.css"
+  },
+  {
+    "revision": "c781a22b96799a20a8d4",
+    "url": "/react-calc/static/js/2.c2a33e40.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-calc/static/js/2.c2a33e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe58795b9e78cb32d7f",
+    "url": "/react-calc/static/js/main.b6c426a4.chunk.js"
+  },
+  {
+    "revision": "316ae6202d6dd97efa2a",
+    "url": "/react-calc/static/js/runtime-main.ef11baa1.js"
+  }
+]);
